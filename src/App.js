cef61dd03ae1cde7cfb7be11f5860f6886b80b42
Raman Andreyuk <romaandreyuk@gmail.com>
2023-12-30 15:48:19 +0100

    effect listening to a keypress Esc

--- src/App.js
+++ src/App.js
@@ -333,6 +333,24 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
   useEffect(
     function () {
       async function getMovieDetails() {
